c9ed09d838bd8705ec1bacf2110ba520bbe6b62f
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-09 12:53:31 +0100

    * Adding function assignedUser

--- app/Http/Controllers/ShipmentsController.php
+++ app/Http/Controllers/ShipmentsController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ShipmentsRequest;
+use App\Http\Requests\UpdateShipment;
 use App\Models\Shipment;
 use App\Models\ShipmentDocuments;
 use App\Traits\ImageUpload;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
 class ShipmentsController extends Controller
 {
-    use ImageUpload;
+    use ImageUpload, AuthorizesRequests;
     public function index()
     {
-        $shipments =  Cache::remember('shipments_unassigned', 300, fn() => Shipment::where('status', Shipment::STATUS_UNASSIGNED)->get());
+        $shipments =  Cache::remember('shipments_unassigned', 300, fn() => Shipment::unassignedshipments()->get());
         return view('shipments.index',
             [
                 'shipments' => $shipments
@@ -22,10 +26,12 @@ class ShipmentsController extends Controller
     }
     public function create()
     {
+        $this->authorize('view', Shipment::class);
         return view('shipments.create_shipment');
     }
     public function store(ShipmentsRequest $request)
     {
+        $this->authorize('create', Shipment::class);
        $shipment = Shipment::create($request->validated());
         foreach ($request->file('documents') as $document) {
             $fileTypes = [
@@ -59,20 +65,31 @@ class ShipmentsController extends Controller
     }
     public function show(Shipment $shipment)
     {
+        Gate::authorize('view', $shipment);
         $shipment->load('shipment_docs');
         return view('shipments.permalink_shipment', compact('shipment'));
     }
-    public function edit(Shipment $shipments)
+    public function edit(Shipment $shipment): View
     {
-        //
+        $this->authorize('edit', Shipment::class);
+        return view('shipments.edit', compact('shipment'));
     }
-    public function update(Request $request, Shipment $shipments)
+    public function update(UpdateShipment $request, Shipment $shipment)
     {
-        //
+        $shipment->update($request->validated());
+        return redirect()->back();
     }
     public function destroy(Shipment $shipments)
     {
         //
+    }
+    public function assignedUser(Request $request, Shipment $shipment): RedirectResponse
+    {
+        $request->validate(['user_id' => 'required|exists:users,id']);
+        $shipment->user_id = $request->user_id;
+        $shipment->status = Shipment::STATUS_STARTED;
+        $shipment->save();
+        return redirect()->back();
     }
 
 }
