52181116a23a91803ae2533ca279f5374bcdc316
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-01-05 09:50:55 +0100

    * Adding vite into layout * Calling livewire from blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body>
